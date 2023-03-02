2e5d7232ec1f025cbb7b32ed014323802d1a9d6f
Maksym Kuziv <mkuziv@hubspot.com>
2023-03-02 20:08:42 +0200

    fixed buttons

--- src/js/components/Started.js
+++ src/js/components/Started.js
@@ -8,7 +8,7 @@ const Started = () => {
         <div>
           <div className='started-title Rifton'>Everything you need to help grow a business is a click away</div>
           <div className='started-buttons'>
-            <a href='https://www.intellihance.biz/'><button className='Montserrat-500 sign-in-started'>Sign In</button></a>
+            <a className='a-signin' href='https://www.intellihance.biz/'><button className='Montserrat-500 sign-in-started'>Sign In</button></a>
             <a className='a-started' href='https://ideaconsult.biz/get-started/'><button className='Montserrat-500 get-started-started'>Get Started</button></a>
           </div>
         </div>
